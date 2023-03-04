eff28b99e7b701301c2127045b5e98b7856baa98
nkersin <nkersin@gmail.com>
2023-03-04 23:01:50 +0900

    회원인증

--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
 const UserSchema = new mongoose.Schema({
@@ -13,7 +14,7 @@ UserSchema.methods.serialize = function() {
     delete data.hashedPassword;
     return data;
   };
-  
+
 UserSchema.methods.setPassword = async function(password) {
   const hash = await bcrypt.hash(password, 10);
   this.hashedPassword = hash;
@@ -28,7 +29,20 @@ UserSchema.statics.findByUsername = function(username) {
   return this.findOne({ username });
 };
 
-
+UserSchema.methods.generateToken = function() {
+    const token = jwt.sign(
+      // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣습니다.
+      {
+        _id: this.id,
+        username: this.username,
+      },
+      process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣습니다.
+      {
+        expiresIn: '7d', // 7일 동안 유효함
+      },
+    );
+    return token;
+  };
 
 const User = mongoose.model('User', UserSchema);
 export default User;
